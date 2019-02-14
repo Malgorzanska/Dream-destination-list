ed9ef0f59525d549abf40b622cf917619794ca51
Małgorzata Dobrzańska <malgorzata.dobrzanska1@gmail.com>
2019-02-14 17:20:11 +0100

    RWD added

--- js/app.js
+++ js/app.js
@@ -2,19 +2,23 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let input = document.querySelector("#place");
     let addBtn = document.querySelector(".addBtn");
+    let moreError = document.querySelector(".more");
+    let lessError = document.querySelector(".less");
 
     addBtn.addEventListener("click", function() {
        let newPlace = input.value;
-       let para = document.querySelector(".error");
 
-       para.classList.add('is-hidden');
+       moreError.classList.add('is-hidden');
+       lessError.classList.add('is-hidden');
 
-       if (newPlace.length < 3){
-           para.classList.remove('is-hidden');
+       if (newPlace.length < 3 ){
+           moreError.classList.remove('is-hidden');
+       } else if (newPlace.length > 12){
+            lessError.classList.remove('is-hidden');
        } else {
-           addItem(newPlace);
-           input.value  = "";
-       }
+            addItem(newPlace);
+            input.value  = "";
+        }
     });
 
     function removeItem(){
@@ -49,14 +53,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
         let undone = document.createElement('button');
         undone.classList.add('undone');
-        undone.innerText = "Remove!";
-
-        undone.addEventListener("click", removeItem );
+        undone.innerText = "Remove";
+        undone.addEventListener("click", removeItem);
 
         let done = document.createElement('button');
         done.classList.add('done');
-        done.innerText = "Done!";
-
+        done.innerText = "Done";
         done.addEventListener("click", moveItem );
 
         buttons.appendChild(done);
@@ -66,13 +68,20 @@ document.addEventListener("DOMContentLoaded", function(){
         newList.appendChild(newItem, newList.childNodes[0]);
     }
 
-
     input.addEventListener('keydown', function (e) {
         let value = this.value;
 
-        if ((e.code === "Enter" || event.code === "NumpadEnter") && value) {
-            addItem(value);
-            input.value  = "";
-        }
+        moreError.classList.add('is-hidden');
+        lessError.classList.add('is-hidden');
+
+         if ((e.code === "Enter" || event.code === "NumpadEnter") && (value.length < 3 ) ) {
+            moreError.classList.remove('is-hidden');
+        } else if ((e.code === "Enter" || event.code === "NumpadEnter") && (value.length > 12) ){
+             lessError.classList.remove('is-hidden');
+        } else if ((e.code === "Enter" || event.code === "NumpadEnter") && value) {
+             addItem(value);
+             input.value  = "";
+         }
+
     });
 });
